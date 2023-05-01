ccd7b8a3f377ac119706a6a291da84429314f629
Taras Kredens <taras.kredens@empeek.com>
2023-05-01 18:43:39 +0300

    fix todo list

--- src/common/components/TodoList.tsx
+++ src/common/components/TodoList.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 
 import Todo from './TodoItem';
 import { useTodosState } from '../../hooks';
 
+const useStyles = createUseStyles({
+  list: {
+    height: 'calc(100vh - 370px)',
+    overflowY: 'auto',
+    padding: '10px 16px',
+  },
+});
+
 const TodoList = () => {
+  const { list } = useStyles();
   const { filteredTodos } = useTodosState();
 
   return (
-    <div className="todo-list">
-      <ul>
-        {filteredTodos.map(todo => (
-          <Todo id={todo.id} todoItem={todo} key={todo.id} />
-        ))}
-      </ul>
+    <div className={list}>
+      {filteredTodos.map(todo => (
+        <Todo id={todo.id} todoItem={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
